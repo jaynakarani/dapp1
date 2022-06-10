aaadc98f0fca700aa269349eaae610f64f0b0d76
jaynakarani <jaynakarani5@gmail.com>
2022-06-10 15:33:19 +0530

    final

--- funding/src/App.js
+++ funding/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Web3 from 'web3';
 import { loadContract } from './utils/load';
 import detectEthereumProvider from "@metamask/detect-provider"
-import contract from '@truffle/contract';
 
 function App() {
   const [web3api, setweb3api] = useState({
@@ -11,30 +10,30 @@ function App() {
     web3: null,
     contract: null
   });
-   
-  const [account,setaccount] =  useState(null)
-  const [balance,setbalance]=useState(null);
-  const [reload,shouldrreload]=useState(false);
 
-const reloadeffect =  ()=>shouldrreload(!reload)
+  const [account, setaccount] = useState(null)
+  const [balance, setbalance] = useState(null);
+  const [reload, shouldrreload] = useState(false);
+
+  const reloadeffect = () => shouldrreload(!reload)
 
   useEffect(() => {
     const loadProvider = async () => {
       // console.log(window.web3);
       // console.log(window.ethereum);
       const provider = await detectEthereumProvider();
-      const contract = await loadContract("funder",provider)
-      if(provider){
-        provider.request({method:"eth_requestAccounts"});
+      const contract = await loadContract("funder", provider)
+      if (provider) {
+        provider.request({ method: "eth_requestAccounts" });
         setweb3api({
           web3: new Web3(provider),
           provider,
           contract
         })
       }
-      else{
+      else {
         console.error("install meta-mask")
-      } 
+      }
       // if (window.ethereum) {
       //   provider = window.ethereum
       //   try {
@@ -51,44 +50,47 @@ const reloadeffect =  ()=>shouldrreload(!reload)
       //   provider = new Web3.providers.HttpProvider("http://localhost:7545")
       // }
 
-      
+
     };
     loadProvider()
   }, []);
-  useEffect(()=>{
-    const loadbalance = async()=>{
-      const{contract,web3}=web3api;
+  useEffect(() => {
+    const loadbalance = async () => {
+      const { contract, web3 } = web3api;
       const balance = await web3.eth.getBalance(contract.address);
-      setbalance(web3.utils.fromWei(balance,"ether"));
+      setbalance(web3.utils.fromWei(balance, "ether"));
     }
-     web3api.contract && loadbalance();
-  },[web3api,reload]);
+    web3api.contract && loadbalance();
+  }, [web3api, reload]);
 
-  const Transferfund = async()=>{
-    const {web3,contract}= web3api;
+  const Transferfund = async () => {
+    let a =document.getElementById("fname").value;
+    //console.log(a);
+    const { web3, contract } = web3api;
     await contract.trasfer({
-      from:account,
-      value:web3.utils.toWei("2","ether"),
+      from: account,
+      value: web3.utils.toWei(a, "ether"),
     });
     reloadeffect();
   }
 
-const withdrawfund = async()=>{
-  const {contract,web3}=web3api;
-  const withdrawAmount = web3.utils.toWei("2","ether");
-  await contract.withdraw(withdrawAmount,{ 
-    from:account
-  })
-  reloadeffect()
-}
+  const withdrawfund = async () => {
+    let a =document.getElementById("fname").value;
+    const { contract, web3 } = web3api;
+    const withdrawAmount = web3.utils.toWei(a, "ether");
+    await contract.withdraw(withdrawAmount, {
+      from: account
+    })
+    reloadeffect()
+  }
 
-  useEffect(()=>{
-    const getaccount = async()=>{
-      const accounts =await web3api.web3.eth.getAccounts()
+  useEffect(() => {
+    const getaccount = async () => {
+      const accounts = await web3api.web3.eth.getAccounts()
       setaccount(accounts[0])
     }
     web3api.web3 && getaccount()
-  },[web3api.web3])
+  }, [web3api.web3])
 
   // console.log(account)
   return (
@@ -98,7 +100,7 @@ const withdrawfund = async()=>{
 
         <div className="card-body">
           <h5 className="card-title">Balance: {balance} ETH </h5>
-          <p className="card-text">Account : {account ? account:"not connected"} </p>
+          <p className="card-text">Account : {account ? account : "not connected"} </p>
           {/* <button type="button" className="btn btn-success mx-1" onClick={async()=>{
           // metamask mathi account address leva
           const accounts = await window.ethereum.request({method:"eth_requestAccounts"})
@@ -106,13 +108,20 @@ const withdrawfund = async()=>{
         }}>
           Connect to metamask
         </button> */}
-          <button type="button" className="btn btn-success mx-1 " onClick={Transferfund} >
+          <div className="form-group">
+            <label htmlFor="exampleFormControlInput1" className="mx-2"><h5>enter amount :</h5></label>
+            <input type="text"  id="fname" name="fname" size="25"/>
+            <label>Ether</label>
+           {/* <div className="container"> <textarea class="form-control" id="exampleFormControlTextarea1" colunms="10"></textarea></div> */}
+          </div>
+          <button type="button" className="btn btn-success  my-2 mx-1 " onClick={Transferfund} >
             Transfer
           </button>
-          <button type="button" className="btn btn-primary mx-1" onClick={withdrawfund}>
+          <button type="button" className="btn btn-primary my-2 mx-1" onClick={withdrawfund}>
             Withdraw
           </button>
         </div>
+
         <div className="card-header">JAY NAKARANI</div>
       </div>
     </>
